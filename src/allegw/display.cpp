42f381707eb7c6b67569126f51ddcca98b06f682
westelh <reo.retriever@gmail.com>
2018-02-03 16:09:05 +0900

    Separate getting display pointer into a function

--- src/allegw/display.cpp
+++ src/allegw/display.cpp
@@ -3,16 +3,19 @@
 #include "headers/app.hpp"
 using namespace allegw;
 
-display::display(): display_ptr{nullptr, deleter{}}, events{nullptr} {
-    al_init_timeout(&timeout, 0.06);
-    if (auto ptr = al_create_display(width, height); ptr != nullptr) { 
-        display_ptr.reset(ptr);
-        if (events = al_create_event_queue(); events != nullptr) {
-            al_register_event_source(events, al_get_display_event_source(ptr));
-        }
-        else throw std::runtime_error{"event queue creation failed."};
+namespace {
+    ALLEGRO_DISPLAY* get_display() {
+        if(auto r = al_create_display(display::width, display::height); r != nullptr)
+            return r;
+        else throw std::runtime_error{"display creation failed."};
     }
-    else throw std::runtime_error{"display creation failed."};
+}
+
+display::display(): display_ptr{get_display(), deleter{}}, events{nullptr} {
+    al_init_timeout(&timeout, 0.06);
+    if (events = al_create_event_queue(); events != nullptr)
+            al_register_event_source(events, al_get_display_event_source(display_ptr.get()));
+    else throw std::runtime_error{"event queue creation failed."};
 }
 
 void display::wait_and_draw() noexcept {
