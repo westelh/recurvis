d0030844975d08e52f0419dedef22593302d9dcc
westelh <reo.retriever@gmail.com>
2018-02-05 15:41:51 +0900

    Drawable class implemented base functional

--- src/rv/drawable.cpp
+++ src/rv/drawable.cpp
@@ -1,1 +1,20 @@
 #include "headers/drawable.hpp"
+using namespace recurvis;
+
+std::unique_lock<std::recursive_mutex> drawable::get_lock() const noexcept {
+    return std::unique_lock<std::recursive_mutex>(mutex);
+}
+
+drawable::drawable(const coord& p): pos{p} {  }
+drawable::drawable(coord&& p): pos{p}{  }
+
+coord drawable::get_pos() const noexcept { return pos; }
+
+void drawable::set_pos(const coord& c) noexcept {
+    auto lock = get_lock();
+    pos = c;
+}
+void drawable::set_pos(coord&& c) noexcept {
+    auto lock = get_lock();
+    pos = c;
+}
--- src/rv/headers/drawable.hpp
+++ src/rv/headers/drawable.hpp
@@ -2,11 +2,24 @@
 #define DRAWABLE_HPP
 
 #include <map>
+#include <mutex>
 
 namespace recurvis {
     using coord = std::pair<int, int>;
+    
     class drawable {
         coord pos;
+        mutable std::recursive_mutex mutex;
+    protected:
+        std::unique_lock<std::recursive_mutex> get_lock() const noexcept;
+    public:
+        drawable(const coord&);
+        drawable(coord&&);
+        ~drawable() = default;
+        coord get_pos() const noexcept;
+        void set_pos(const coord&) noexcept;
+        void set_pos(coord&&) noexcept;
+        virtual void draw() = 0;
     };
 }
 
