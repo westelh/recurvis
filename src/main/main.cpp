c150d0e2242172ae8dd471b9e9a64a6937be11a4
westelh <reo.retriever@gmail.com>
2018-02-18 11:07:06 +0900

    Run drawing flow on other thread
    - Run display loop on std::thread.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,15 +1,19 @@
+#include <thread>
 #include <allegro5/allegro.h>
 #include "../allegw/headers/app.hpp"
 #include "../rv/headers/card.hpp"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
     recurvis::allegw::app app;
-    auto display = app.make_display();
     auto card = recurvis::card{};
-    while(!display.is_time_to_close()) {
-        display.clear();
-        card.draw();
-        display.wait_and_draw();
-    }
+    std::thread th{[&](){
+        auto display = app.make_display();
+        while(!display.is_time_to_close()) {
+            display.clear();
+            card.draw();
+            display.wait_and_draw();
+        }
+    }};
+    th.join();
     return true;
 }
