8ceb6c11270622f51e85911b08a575f7c6be38f3
Unknown <reo.retriever@gmail.com>
2018-04-08 12:05:14 +0900

    chore: Add allegro as submodule
    - Init allegro repo at 3rd/allegro as a sumodule
    - Build configuration changed for new library path

--- install_allegro.sh
+++ install_allegro.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/liballeg/allegro5.git --depth=1 -b5.2.3.0
-cd allegro5
+git clone https://github.com/liballeg/allegro5.git 3rd/allegro --depth=1 -b5.2.3.0
+cd 3rd/allegro
 cmake -DWANT_AUDIO=OFF\
     -DWANT_DEMO=OFF\
     -DWANT_EXAMPLES=OFF\
