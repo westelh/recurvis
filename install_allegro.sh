daabdb200ffc6a17b2932627f75962c08fc5aa35
westelh <westelh@gmail.com>
2020-03-12 23:42:42 +0900

    Fix compile phase in build routine

--- install_allegro.sh
+++ install_allegro.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 git clone https://github.com/liballeg/allegro5.git 3rd/allegro --depth=1 -b5.2.3.0
 cd 3rd/allegro
 cmake -DWANT_AUDIO=OFF\
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 gradle build
-DYLD_LIBRARY_PATH="./allegro5/build/lib:./build/libs/allegro_wrapper/shared/debug/" build/exe/main/debug/main
+DYLD_LIBRARY_PATH="./3rd/allegro/build/lib:./build/libs/allegro_wrapper/shared/release/" build/exe/main/release/main
