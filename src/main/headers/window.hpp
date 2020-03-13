39f7d37b4a3fd38e60b25a417f0fa86e8d2f9abf
westelh <westelh@gmail.com>
2020-03-13 21:21:10 +0900

    Refactoring window.hpp

--- src/main/headers/window.hpp
+++ src/main/headers/window.hpp
@@ -4,16 +4,27 @@
 #include <functional>
 #include <atomic>
 
+/*
+/////////////////////
+    window class
+/////////////////////
+    
+*/
 class window {
 private:
     std::atomic_bool close_flag_m;
     std::thread update_thread_m;
 protected:
+    // called window update loop
     virtual void update() = 0;
+
 public:
     window(int w, int h);
     virtual ~window();
 
+    // close window immediately
     virtual void close() noexcept;
+
+    // close window and wait until window update loop stops
     virtual void close_and_wait() noexcept;
 };
