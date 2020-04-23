f0e4a0cf743822c34eec1b7d3f39f08b9d8b5432
westelh <westelh@gmail.com>
2020-04-23 17:46:16 +0900

    Code cleanup

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
 #include <memory>
-#include <vector>
 #include <opengl_app.h>
 #include "app_interface.hpp"
-#include "window_updater.h"
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
     auto app = std::unique_ptr<app_interface>(new opengl_app{});
