18aaefdb6d6590d72b0eb175d8227c75e0c865fc
westelh <westelh@gmail.com>
2020-04-24 16:41:04 +0900

    Add vulkan

--- src/ui/cpp/opengl_app.cpp
+++ src/ui/cpp/opengl_app.cpp
@@ -9,7 +9,42 @@ opengl_app::opengl_app() : app_interface() {
     if (initialized) return;
 
     if (!glfwInit()) throw glfw_error::since_last_call();
-    else initialized = true;
+
+    if (!glfwVulkanSupported()) throw glfw_error::since_last_call();
+
+    [[maybe_unused]] auto pfnCreateInstance = (PFN_vkCreateInstance)
+            glfwGetInstanceProcAddress(nullptr, "vkCreateInstance");
+
+    VkApplicationInfo applicationInfo = {
+            VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            nullptr,
+            "appName",
+            1,
+            "engineName",
+            1,
+            0
+    };
+
+    VkInstanceCreateInfo instanceCreateInfo = {
+            VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            nullptr,
+            0,
+            &applicationInfo,
+            0,
+            nullptr,
+            0,
+            nullptr
+    };
+
+    VkInstance instance;
+
+    if (vkCreateInstance(&instanceCreateInfo, nullptr, &instance) != VK_SUCCESS)
+        throw std::runtime_error("failed to create instance");
+
+    [[maybe_unused]] PFN_vkCreateDevice pfnCreateDevice = (PFN_vkCreateDevice)
+            glfwGetInstanceProcAddress(instance, "vkCreateDevice");
+
+    initialized = true;
 }
 
 std::unique_ptr<window_interface> opengl_app::create_window(int w, int h) {
